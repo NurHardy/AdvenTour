3e9cac5f19377ac80f3adcfa7bb66ed27349d461
aadddrr <diamondsion@gmail.com>
2016-04-01 16:14:58 +0700

    sync

--- web/application/models/MVenue.php
+++ web/application/models/MVenue.php
@@ -25,37 +25,18 @@
 		
 		public function getVenues()
 		{
+			$this->db->join('venue_photo', 'venue_photo.idVenue = venue.idVenue');
 			$query = $this->db->get('venue');;
 			return $query->result_array();
 		}
 
-		public function insertUser()
+		public function getGoAdventour($time, $category, $budget)
 		{
-				$this->username	= $this->input->post('username');
-				$this->email  	= $this->input->post('email');
-				$this->password = $this->input->post('password');
-				$this->name  	= $this->input->post('name');
-				$this->exp  	= $this->input->post('exp');
-				$this->level  	= $this->input->post('level');
-				$this->token  	= $this->input->post('token');
-				$this->photo  	= $this->input->post('photo');
-
-				$this->db->insert('user', $this);
+			$this->db->join('venue_photo', 'venue_photo.idVenue = venue.idVenue');
+			$this->db->join('travel_journal', 'travel_journal.idVenue = venue.idVenue');
+			$this->db->where('cost <=', $budget);
+			$query = $this->db->get('venue');;
+			return $query->result_array();
 		}
-
-		public function updateUser($idUser)
-		{
-				$this->username	= $this->input->post('username');
-				$this->email  	= $this->input->post('email');
-				$this->password = $this->input->post('password');
-				$this->name  	= $this->input->post('name');
-				$this->exp  	= $this->input->post('exp');
-				$this->level  	= $this->input->post('level');
-				$this->token  	= $this->input->post('token');
-				$this->photo  	= $this->input->post('photo');
-				
-				$this->db->update('user', $this, array('idUser' => $idUser));
-		}
-
 	}
 ?>
